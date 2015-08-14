34c7f5b7d5fa07132917810f7b075dc2c29153bf
涵曦 <hanxi.info@gmail.com>
2015-08-14 23:35:24 +0800

    bugfixed for post response json.

--- file-server.py
+++ file-server.py
@@ -15,7 +15,7 @@ import mimetypes
 import json
 
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding("utf-8")
 mimetypes.init()
 
 g_filepath = ""
@@ -24,31 +24,31 @@ def transDicts(params):
     dicts={}
     if len(params)==0:
         return
-    params = params.split('&')
+    params = params.split("&")
     for param in params:
-        keyvalue = param.split('=')
+        keyvalue = param.split("=")
         key = keyvalue[0]
         value = keyvalue[1]
-        value = urllib.unquote_plus(value).decode("utf-8", 'ignore')
+        value = urllib.unquote_plus(value).decode("utf-8", "ignore")
         dicts[key] = value
     return dicts
 
 class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def end_headers (self):
-        self.send_header('access-control-allow-origin', '*')
+        self.send_header("access-control-allow-origin", "*")
         BaseHTTPServer.BaseHTTPRequestHandler.end_headers(self)
 
     def do_GET(self):
         query = urllib.splitquery(self.path)
-        path = urllib.unquote_plus(query[0]).decode("utf-8", 'ignore')
+        path = urllib.unquote_plus(query[0]).decode("utf-8", "ignore")
         queryParams = {}
 
-        if '?' in self.path:
+        if "?" in self.path:
             if query[1]:
                 queryParams = transDicts(query[1])
 
         fn = "%s%s" % (g_filepath, path)
-        fn = urllib.unquote_plus(fn).decode("utf-8", 'ignore')
+        fn = urllib.unquote_plus(fn).decode("utf-8", "ignore")
         fn = fn.replace("/",os.sep)
 
         content = ""
@@ -69,7 +69,7 @@ class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                     if os.path.isdir(filepath):
                         filename += os.sep
                     mtime = os.path.getmtime(filepath)
-                    filelist.append({'filename':filename,'mtime':mtime})
+                    filelist.append({"filename":filename,"mtime":mtime})
             content = json.dumps(filelist)
         else:
             print(g_filepath, path, fn)
@@ -84,14 +84,14 @@ class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         path = query[0]
         queryParams = {}
 
-        if '?' in self.path:
+        if "?" in self.path:
             if query[1]:
                 queryParams = transDicts(query[1])
 
-        content = "{result=0, msg = 'OK'}"
-        if path=='/upload':
+        resultdict = {"result":0, "msg":"OK"}
+        if path=="/upload":
             if queryParams.has_key("name"):
-                filesize = int(self.headers['content-length'])
+                filesize = int(self.headers["content-length"])
                 filecontent = self.rfile.read(filesize)
                 fn = queryParams["name"]
                 fn = "%s%s" % (g_filepath, fn)
@@ -99,16 +99,20 @@ class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 if not os.path.exists(dirname):
                     os.makedirs(dirname)
                 if os.path.isdir(fn):
-                    content = "{result=1, msg = 'File name is directory.'}"
+                    resultdict.result = 1
+                    resultdict.msg = "File name is directory."
                 else:
-                    f = open(fn,'wb')
+                    f = open(fn,"wb")
                     f.write(filecontent)
                     f.close()
             else:
-                content = "{result=2, msg = 'Need file name.'}"
+                resultdict.result = 2
+                resultdict.msg = "Need file name."
         else:
-            content = "{result=3, msg = 'No this API.'}"
+            resultdict.result = 3
+            resultdict.msg = "No this API."
 
+        content = json.dumps(resultdict)
         self.send_response(200)
         self.send_header("content-type","application/json")
         self.end_headers()
@@ -118,12 +122,12 @@ class ThreadingHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer
     pass
 
 def run(port):
-    print 'HTTP File Server Started at port:', port
-    server_address = ('', port)
-    httpd = ThreadingHTTPServer(('', port), HTTPRequestHandler)
+    print "HTTP File Server Started at port:", port
+    server_address = ("", port)
+    httpd = ThreadingHTTPServer(("", port), HTTPRequestHandler)
     httpd.serve_forever()
 
-if __name__=='__main__':
+if __name__=="__main__":
     g_filepath = "./files/"
     if len(sys.argv)>=2:
         g_filepath = sys.argv[1]
